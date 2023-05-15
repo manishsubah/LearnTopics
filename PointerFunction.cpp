6a8dff323d8e7e21d2f95eede0af56302b3c1e1e
Manish Ranjan <manishranjan.soft@gmail.com>
2023-05-15 16:06:12 +0530

    pointerfunction

--- PointerFunction.cpp
+++ PointerFunction.cpp
@@ -1,9 +1,6 @@
 #include<iostream>
 using namespace std;
 
-int cube(int x) {
-    return x*x*x;
-}
 bool switchFan(string str) {
     if(str == "true") {
         return true;
@@ -11,21 +8,14 @@ bool switchFan(string str) {
     return false;
 }
 int main() {
-    int num = 4;
-    int result;
-    
-
-    int (*funptr)(int);
-    funptr = cube;
-
-    result = (*funptr)(num);
-
-    cout<<"The volume of cube: "<<result<<endl;
 
     string fan = "true";
+
     bool (*swhfan)(string);
+
     swhfan = switchFan;
     bool operation = (*swhfan)(fan);
+    
     cout<<"Fan is on: "<<operation<<endl;
 
     return 0;
